b5332f5c60f8038d2d90d958bb09a08f653652af
CedricG44 <cedric-garcia@sfr.fr>
2019-10-18 17:39:52 +0200

    TP7 React Redux

---
+++ 07-react-native-livres/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ListeLivres from "./app/ListeLivres";
+import globalReducer from "./app/reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(globalReducer);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ListeLivres />
+      </Provider>
+    );
+  }
+}
---
+++ 07-react-native-livres/app/reducers/livreReducer.js
@@ -0,0 +1,28 @@
+import uuid from "uuid";
+import { AJOUTER_LIVRE, SUPPRIMER_LIVRE } from "../actions";
+
+const etatInitial = {
+  livres: [{ nom: "Livre 1", auteur: "Auteur 1", id: uuid() }]
+};
+
+const livreReducer = (etat = etatInitial, action) => {
+  switch (action.type) {
+    case AJOUTER_LIVRE:
+      const { livre: nouveauLivre } = action;
+
+      nouveauLivre.id = uuid();
+      return {
+        livres: [...etat.livres, nouveauLivre]
+      };
+    case SUPPRIMER_LIVRE:
+      const { livre: livreASupprimer } = action;
+
+      return {
+        livres: etat.livres.filter(livre => livre.id !== livreASupprimer.id)
+      };
+    default:
+      return etat;
+  }
+};
+
+export default livreReducer;
